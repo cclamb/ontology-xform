98054474802bc9066f7143d3cd669d5f45b86f5b
michael <michael@M11x-Linux.(none)>
2012-02-25 15:31:59 -0700

    will create empty Entity and Property classes

--- lib/flounder/description_builder.rb
+++ lib/flounder/description_builder.rb
@@ -5,33 +5,57 @@ module Flounder
 	
 	class Ontology < XML::SAX::Document
 		def initialize		
-			@elements = Array.new
-			@description
+			@element = Hash.new
+			@properties = Array.new
+			@entities = Array.new
 		end
+
 		
-		def start_element(element, attributes)
-			if element == 'owl:Class'
-				attributes.each { |att|
-					if att[0] == 'ref:about'
-						@elements << att[1]
-					end
-				}
+		def start_element(name, attributes)
+			case name
+				when 'owl:Class'
+					@element[:name] = attributes[0][1]
+				when 'rdfs:subClassOf'
+					@element[:parent] = attributes[0][1]
 			end
 		end
 
-		def end_document
 
-			@elements.each {|e|
-				@description = String.new
-				@description = @description + self.build_class(e)
-			}
-		
-			puts @description
+		def end_element(name)
+			if name == 'owl:Class'
+				if @element[:parent] == nil
+					@entities << @element[:name]
+				else
+					@properties << @element
+					@element = Hash.new
+				end
+			end			
 		end
 
-		def build_class(name)
-			class_string = "class #{name.capitalize}\n\tdef initialize\n\tend\nend\n\n"
-			return class_string
+
+		def end_document
+			@description = "#Listing Entities\n"	
+			@entities.each {|e|
+				@description = @description + self.build_entity(e)
+			}
+			@description = @description + "#Listing Properties\n"
+			@properties.each {|p|
+				if @entities.include?(p[:parent]) == true
+					@description = @description + self.build_empty_property(p[:name])
+				end
+			}
+			File.open('ontological description.rb', 'w') {|f| f.write(@description) }
+		end
+
+
+		def build_empty_property(name)
+			prop_string = "class #{name.capitalize} < ComparableProperty\n\tdef initialize (value)\n\t\t@value = value\n\tend\nend\n\n"
+			return prop_string
+		end
+
+		def build_entity(name)
+			e_string = "class #{name.capitalize} < Entity\n\tdef initialize(property_list)\n\t\t@properties = property_list\n\tend\nend\n\n"
+			return e_string
 		end
 	end
 
@@ -52,5 +76,37 @@ end
 
 
 #test code
+#test = Flounder::DescriptionBuilder.new('../../etc/Ontology.owl')
+#test = Flounder::DescriptionBuilder.new('../../etc/Ontology2.owl')
 test = Flounder::DescriptionBuilder.new('../../etc/simple-ontology.owl')
 test.generate_description
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
