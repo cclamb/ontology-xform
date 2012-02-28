19e5d13d0b5d1a66efbbc9a5c92dca0205154774
Chris Lamb <cclamb@cs.unm.edu>
2012-02-27 18:41:13 -0700

    work

---
+++ etc/scratch.rb
@@ -0,0 +1,17 @@
+require 'nokogiri'
+
+class Dispatcher < Nokogiri::XML::SAX::Document
+
+  def start_element(name, attributes)
+    puts "Name: <#{name}> Attributes: <#{attributes}>\n"
+  end
+
+  def error(msg)
+    puts "ERROR! ==> #{msg}\n"
+  end
+
+end
+
+parser = Nokogiri::XML::SAX::Parser.new Dispatcher.new
+parser.parse_file 'simple-ontology.owl'
+#parser.parse File.read 'simple-ontology.owl'
