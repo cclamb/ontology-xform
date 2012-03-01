9c0ce84e294a7cbcee9fd919dae2666242ae835f
cclamb <cclamb@cs.unm.edu>
2012-03-01 06:36:45 -0700

    functional arch with data

--- etc/scratch.rb
+++ etc/scratch.rb
@@ -1,6 +1,18 @@
 require 'nokogiri'
 
-def process_element(clz)
+class Dispatcher < Nokogiri::XML::SAX::Document
+
+  def start_element name, attributes
+    puts "<=== Name: #{name} started ===>\n"
+  end
+
+  def error msg
+    puts "++++ Holy Smokes Batman! #{msg}\n"
+  end
+
+end
+
+def process_element clz
   
   puts "Attributes: #{clz.attributes.size}\n"
   clz.attributes.each do |key, value|
@@ -16,11 +28,27 @@ def process_element(clz)
   
 end
 
-xml = File.read 'simple-ontology.owl'
-doc = Nokogiri::XML xml
-clz = doc.xpath '//owl:Class'
-clz.each do |clz|
-  puts "\n\n<=== Begin Class <#{clz.name}> ===>"
-  process_element clz
-  puts "<=== End Class <#{clz.name} ===>"
+def process_doc doc
+  clz = doc.xpath '//owl:Class'
+  clz.each do |clz|
+    puts "\n\n<=== Begin Class <#{clz.name}> ===>"
+    process_element clz
+    puts "<=== End Class <#{clz.name} ===>"
+  end
 end
+
+xml = File.read 'simple-ontology.owl'
+
+doc = Nokogiri::XML xml
+
+process_doc doc
+
+#sax = Nokogiri::XML::SAX::Parser.new Dispatcher.new
+
+#doc.namespaces.each do |k,v|
+#  puts "<#{k}, #{v}>\n"
+#end
+
+#sax.parse(doc.to_xml) do |ctx|
+#  ctx.replace_entities = false
+#end
