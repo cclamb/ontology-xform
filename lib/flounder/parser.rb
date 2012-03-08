02a13f99d802eca7b5a55526da6dbad94fd9ed13
cclamb <cclamb@cs.unm.edu>
2012-03-08 06:16:58 -0700

    added parser and tests

--- lib/flounder/parser.rb
+++ lib/flounder/parser.rb
@@ -6,12 +6,13 @@ module Flounder
 
     def initialize
       @elements = {}
+      @structure = {}
     end
 
     def parse xml
       @doc = Nokogiri::XML xml
       parse_classes
-      return {:elements => @elements}
+      return {:elements => @elements, :structure => @structure}
     end
 
     private
@@ -23,11 +24,21 @@ module Flounder
         c.attributes.each do |key, value|
           attribs[key.to_sym] = value.to_s
         end
+
         full_type_name = attribs[:about]
         @elements[full_type_name.to_sym] = attribs
+
+        sub_elements = c.xpath 'rdfs:subClassOf'
+        if sub_elements.size > 0
+          name = full_type_name.to_sym
+          parent = sub_elements[0].attribute('resource').to_s.to_sym
+          #val = {:name => name, :parent => parent }
+          #@structure.push val
+          @structure[name] = parent
+        end
       end
     end
-
+    
   end
 
 end
--- test/specs/parser_spec.rb
+++ test/specs/parser_spec.rb
@@ -2,7 +2,41 @@ require 'rspec'
 
 require_relative '../../lib/flounder/parser'
 
-SIMPLE_ONTOLOGY_FILE_LOCATION = 'etc/simple-ontology.owl'
+Simple_Test = {
+  :filename => 'etc/simple-ontology.owl',
+  :element_size => 6,
+  :structure_size => 4
+}
+
+Complex_Test = {
+  :filename => 'etc/Ontology.owl',
+  :element_size => 17,
+  :structure_size => 15 
+}
+
+Complex_Test_2 = {
+  :filename => 'etc/Ontology2.owl',
+  :element_size => 17,
+  :structure_size => 15 
+}
+
+def elements_test(filename, size)
+  builder = Flounder::Parser.new
+  xml = File.read filename
+  results = builder.parse xml
+  elems = results[:elements]
+  elems.should_not eq nil
+  elems.size.should eq size
+end
+
+def structure_test(filename, size)
+  builder = Flounder::Parser.new
+  xml = File.read filename
+  results = builder.parse xml
+  structure = results[:structure]
+  structure.should_not eq nil
+  structure.size.should eq size
+end
 
 describe Flounder::Parser do
 
@@ -10,16 +44,41 @@ describe Flounder::Parser do
     builder = Flounder::Parser.new
     builder.should_not eq nil
   end
-  
-  it 'should return unstructured elements with their attributes' do
-    builder = Flounder::Parser.new
-    xml = File.read SIMPLE_ONTOLOGY_FILE_LOCATION
-    results = builder.parse xml
-    elems = results[:elements]
-    elems.should_not eq nil
-    elems.size.should eq 6
+
+  context 'Simple Ontology (simple-ontology.owl)' do
+      
+    it 'should return unstructured elements with their attributes' do
+      elements_test Simple_Test[:filename], Simple_Test[:element_size]
+    end
+
+    it 'should return structure information keyed by class name' do
+      structure_test Simple_Test[:filename], Simple_Test[:structure_size]
+    end
+
   end
 
-  it 'should return structure information keyed by class name'
+  context 'Complex Ontology (Ontology.owl)' do
+
+    it 'should return unstructured elements with their attributes' do
+      elements_test Complex_Test[:filename], Complex_Test[:element_size]
+    end
+
+    it 'should return structure information keyed by class name' do
+      structure_test Complex_Test[:filename], Complex_Test[:structure_size]
+    end
+
+  end
+
+  context 'Complex Ontology (Ontology2.owl)' do
+
+    it 'should return unstructured elements with their attributes' do
+      elements_test Complex_Test_2[:filename], Complex_Test_2[:element_size]
+    end
+
+    it 'should return structure information keyed by class name' do
+      structure_test Complex_Test_2[:filename], Complex_Test_2[:structure_size]
+    end
+
+  end
 
 end
