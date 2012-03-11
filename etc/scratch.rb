b92d91d94cb8c189a004cc027e9fead6f1a28651
cclamb <cclamb@cs.unm.edu>
2012-03-11 09:51:50 -0600

    added parser and tests

--- etc/scratch.rb
+++ etc/scratch.rb
@@ -12,35 +12,58 @@ r = p.parse xml
 elements = r[:elements]
 structure = r[:structure]
 
-elements.each do |k,v|
-  superclazz = structure[k.to_sym]
-  print "#{k}"
-  print " : #{superclazz}" unless superclazz == nil
-  print "\n\n"
-  print "#{v}\n\n"
-end
+#elements.each do |k,v|
+#  superclazz = structure[k.to_sym]
+#  print "#{k}"
+#  print " : #{superclazz}" unless superclazz == nil
+#  print "\n\n"
+#  print "#{v}\n\n"
+#end
 
 class ModuleGenerator
 
   attr_accessor :name, :elements
 
   def initialize
+    name = ''
+    elements = []
     yield self if block_given?
   end
 
   def generate
     print "module #{name}\n\n"
     elements.each { |e| e.generate }
-    print "\n\nend\n\n"
+    print "end\n\n"
+  end
+
+end
+
+class MethodGenerator
+
+  attr_accessor :name, :instructions
+
+  def initialize
+    name = ''
+    instructions = []
+    yield self if block_given?
+  end
+
+  def generate
+    print "def #{name}\n"
+    instructions.each { |i| print "\t#{i}\n"}
+    print "end\n\n"
   end
 
 end
 
 class ClassGenerator
 
-  attr_accessor :name, :parent
+  attr_accessor :name, :parent, :included_module
 
   def initialize
+    name = ''
+    parent = ''
+    included_module = nil
     yield self if block_given?
   end
 
@@ -48,19 +71,38 @@ class ClassGenerator
     print "class #{name}"
     print " < #{parent}" if parent
     print "\n"
-    print "\n\nend"
+    print "\tinclude #{included_module.name}" if included_module
+    print "\n\nend\n\n"
   end
 
+end
+
+im = ModuleGenerator.new do |ctx|
+  ctx.name = 'Mako'
+
+  m = MethodGenerator.new do |ctx|
+    ctx.name = 'print_foo'
+    ctx.instructions = ["puts 'foo'"]
+  end
+
+  ctx.elements = [m]
 end
 
 g = ClassGenerator.new do |ctx|
   ctx.name = 'Name'
   ctx.parent = 'Parent'
+  ctx.included_module = im
 end
 
 m = ModuleGenerator.new do |ctx|
   ctx.name = 'ModuleName'
-  ctx.elements = [g]
+  ctx.elements = [im, g]
+end
+
+module Foo
+  def bar msg
+    puts "#{msg} : #{@blech}"
+  end
 end
 
 m.generate
