c10474c3ff4e8760253e1c0d1bcbecf48fa17e3c
Chris Lamb <cclamb@cs.unm.edu>
2012-03-15 22:12:24 -0400

    work

--- etc/scratch.rb
+++ etc/scratch.rb
@@ -9,20 +9,10 @@ filename = 'simple-ontology.owl'
 p = Parser.new
 xml = File.read filename
 r = p.parse xml
-puts "#{r[:structure]}\n\n#{r[:elements]}\n\n"
+#puts "#{r[:structure]}\n\n#{r[:elements]}\n\n"
 
 elements = r[:elements]
 structure = r[:structure]
-
-# elements.each do |k,v|
-#   superclazz = structure[k.to_sym]
-#   print "#{k}"
-#   print " : #{superclazz}" unless superclazz == nil
-#   print "\n\n"
-#   print "#{v}\n\n"
-# end
-
-# order elements
 
 module_elements = {}
 elements.each do |k, v|
@@ -33,13 +23,18 @@ elements.each do |k, v|
   module_elements[k.to_sym] = clazz
 end
 
+new_elements = []
 structure.each do |k,v|
   clazz = module_elements[k]
-  clazz.parent = module_elements[v] unless clazz == nil
+  unless clazz == nil
+    clazz.parent = module_elements[v]
+    new_elements.push clazz.parent unless new_elements.include? clazz.parent
+  end
+  new_elements.push clazz
 end
 
 mod = ModuleGenerator.new do |ctx|
-  ctx.elements = module_elements.values
+  ctx.elements = new_elements #module_elements.values
   ctx.name = 'SomeMod'
 end
 
@@ -48,8 +43,8 @@ mod.generate str
 
 str = str.gsub! /#|\/|:|\./, '_' 
 
-#puts str
+puts str
 
-#Object.new.instance_eval str
+Object.new.instance_eval str
 
 
