d64eb4e5d978ba0409bdc66eb856e94b857d1d1c
Chris Lamb <cclamb@cs.unm.edu>
2012-03-16 11:40:07 -0400

    rectifier coded, initial mod/class tests in place

--- lib/flounder/rectifier.rb
+++ lib/flounder/rectifier.rb
@@ -4,7 +4,47 @@ module Flounder
   class Rectifier
 
     def rectify elements, structure
-      
+      module_elements = {}
+      elements.each do |k, v|
+        clazz = ClassGenerator.new do |ctx|
+          ctx.name = k.to_s
+          #ctx.parent = v.to_s unless v == nil
+        end
+        module_elements[k.to_sym] = clazz
+      end
+
+      new_elements = []
+      structure.each do |k,v|
+        clazz = module_elements[k]
+        elems = find_parent clazz, module_elements, structure
+        elems = [clazz] + elems
+        elems.reverse.each do |x|
+          new_elements.push x unless new_elements.include? x
+        end
+      end
+
+      mod = ModuleGenerator.new do |ctx|
+        ctx.elements = new_elements #module_elements.values
+        ctx.name = 'SomeMod'
+      end
+
+      str = ''
+      mod.generate str
+
+      str.gsub! /#|\/|:|\./, '_'
+    end
+
+    private
+
+    def find_parent clazz, catalog, structure
+      return [] if clazz == nil
+      #puts clazz.name
+      parent_name = structure[clazz.name.to_sym]
+      #puts "pname : #{parent_name}"
+      return [] if parent_name == nil
+      parent = catalog[parent_name]
+      clazz.parent = parent
+      return [parent] + find_parent(parent, catalog, structure)
     end
 
   end
--- test/specs/rectifier_spec.rb
+++ test/specs/rectifier_spec.rb
@@ -15,14 +15,23 @@ complex_test_2 = {
   :filename => 'etc/Ontology2.owl',
 }
 
-def merge filename
-  parser = Flounder::Parser.new
-  xml = File.read filename
-  results = parser.parse xml
-  rectifier = Flounder::Rectifier.new
-  rectified_results = rectifier.rectify results[:elements], results[:structure]
-  #puts "Results: #{rectified_results}"
-  fail 'unimplemented'
+module RectifierSpec
+
+  def RectifierSpec.merge filename
+    parser = Flounder::Parser.new
+    xml = File.read filename
+    results = parser.parse xml
+    rectifier = Flounder::Rectifier.new
+    rectifier.rectify results[:elements], results[:structure]
+  end
+
+  class SimpleTester
+    def test
+      s = SomeMod::Subject.new
+      s.should_not eq nil
+    end
+  end
+
 end
 
 describe Flounder::Rectifier do
@@ -35,7 +44,10 @@ describe Flounder::Rectifier do
   context 'with a simple ontology' do
 
     it 'should be able to merge the structures and elements' do
-      merge simple_test[:filename]
+      od = RectifierSpec::merge simple_test[:filename]
+      test_ctx = RectifierSpec::SimpleTester.new
+      test_ctx.instance_eval od
+      test_ctx.instance_exec { test }
     end
 
   end
@@ -43,7 +55,8 @@ describe Flounder::Rectifier do
   context 'with a complex Ontology (Ontology.owl)' do
 
     it 'should be able to merge the structures and elements' do
-      merge complex_test[:filename]
+      od = RectifierSpec::merge complex_test[:filename]
+      Object.new.instance_eval od
     end
 
   end
@@ -51,7 +64,8 @@ describe Flounder::Rectifier do
   context 'with a complex Ontology (Ontology2.owl)' do
 
     it 'should be able to merge the structures and elements' do
-      merge complex_test_2[:filename]
+      od = RectifierSpec::merge complex_test_2[:filename]
+      Object.new.instance_eval od
     end
 
   end
