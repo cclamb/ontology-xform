c20a7ab361c6f888784c718cb060f7cb66dc2b45
cclamb <cclamb@cs.unm.edu>
2012-03-20 06:15:41 -0600

    working code generation with Comparable

--- etc/scratch.rb
+++ etc/scratch.rb
@@ -3,7 +3,7 @@ require_relative '../lib/flounder/parser'
 include Flounder
 
 #filename = 'Ontology2.owl'
-filename = 'Ontology.owl'
+filename = 'Ontology2.owl'
 #filename = 'simple-ontology.owl'
 
 p = Parser.new
@@ -12,6 +12,32 @@ r = p.parse xml
 
 elements = r[:elements]
 
+str = ''
 elements.each do |e|
-  puts "===\n#{e}\n==="
+  clazz_name = e[0].split('#').last
+  str << "class #{clazz_name}\n"
+  str << "\tinclude Comparable\n\tdef <=> obj\n" unless e[1][:properties].size == 0
+  e[1][:properties].each do |p|
+    
+    if p[:property] == 'isGreaterThan'
+      str << "\t\treturn 1 if obj.class == #{p[:object]}\n"
+    elsif p[:property] == 'isLowerThan'
+      str << "\t\treturn -1 if obj.class == #{p[:object]}\n"
+    end
+    
+  end
+  str << "\tend\n" unless e[1][:properties].size == 0
+  str << "end\n\n" 
 end
+
+puts str
+
+eval str
+
+ts = Top_Secret.new
+s = Secret.new
+u = Unclassified.new
+
+puts u < s
+puts s < u
+puts ts <=> u
