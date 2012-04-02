104e1f3bb51e0eabd1b7645a6fc78bb8bfda17ee
cclamb <cclamb@cs.unm.edu>
2012-04-02 06:33:47 -0600

    work

--- etc/rdf-exp.rb
+++ etc/rdf-exp.rb
@@ -1,5 +1,22 @@
 require 'rdf'
-require 'rdf/rdfxml'
+#require 'rdf/rdfxml'
+
+include RDF
+
+require 'rdf/ntriples'
+
+graph = RDF::Graph.load("http://rdf.rubyforge.org/doap.nt")
+query = RDF::Query.new({
+  :person => {
+    RDF.type  => FOAF.Person,
+    FOAF.name => :name,
+    FOAF.mbox => :email,
+  }
+})
+
+query.execute(graph).each do |solution|
+  puts "name=#{solution.name} email=#{solution.email}"
+end
 
 # RDF::RDFXML::Reader.open("./Ontology2.owl") do |reader|
 #   reader.each_statement do |statement|
@@ -7,23 +24,23 @@ require 'rdf/rdfxml'
 #   end
 # end
 
-filename = 'Ontology2.owl'
+#filename = 'Ontology2.owl'
 # filename = 'Ontology4.owl'
 # filename = 'simple-ontology.owl'
 
 # xml = File.read filename
 
 #g = RDF::Graph.load 'file:///home/cclamb/Work/ontology-xform/etc/Ontology2.owl'
-g = RDF::Graph.load("http://rdf.rubyforge.org/doap.nt")
+#g = RDF::Graph.load("http://rdf.rubyforge.org/doap.nt")
 # q = RDF::Query.new ({
 #   :class => {
 #     :about => 'http://www.semanticweb.org/ontologies/2012/1/Ontology.owl#Context'
 #   }
 # })
 
-g.each do |e|
-  puts e.inspect
-end
+# g.each do |e|
+#   puts e.inspect
+# end
 
 # q.execute(g).each do |solution|
 #   puts solution.inspect
